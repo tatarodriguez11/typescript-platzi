dc31b5e2800ecfda2d9a1217894a41157be9e0a9
tatarodriguez11 <rodriguezjessicatatiana@gmail.com>
2019-07-15 19:14:57 -0500

    bug fixes

--- src/app/player-dialog/player-dialog.component.ts
+++ src/app/player-dialog/player-dialog.component.ts
@@ -3,7 +3,7 @@ import { Countries, SquadNumber, Player } from '../interfaces/player';
 import { PlayerService } from '../services/player.service';
 import { TeamService } from '../services/team.service';
 import { take } from 'rxjs/operators';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-player-dialog',
@@ -21,7 +21,20 @@ export class PlayerDialogComponent implements OnInit {
       label: key,
       key: SquadNumber[key]
     }));
-  constructor(private playerService: PlayerService, private teamService: TeamService) {}
+
+
+    playerForm: FormGroup = this.form.group({
+      name: ['', [Validators.minLength(3), Validators.requiredTrue]],
+      lastName: ['', [Validators.minLength(3), Validators.requiredTrue]],
+      position: [''],
+      weight: ['', Validators.required],
+      height: ['', Validators.required],
+      nationality: [''],
+      leftFooted: ['']
+
+    });
+
+  constructor( private form: FormBuilder, private playerService: PlayerService, private teamService: TeamService) {}
 
   ngOnInit() {
     this.teamService
@@ -32,6 +45,18 @@ export class PlayerDialogComponent implements OnInit {
           this.team = teams[0];
         }
       });
+
+    console.log('error1', this.playerForm.errors);
+    console.log('error2', this.playerForm.getError);
+    console.log('error3', this.playerForm.hasError);
+
+   // console.log(this.playerForm.contains('name'));
+    if (this.player) {
+        const controls = this.playerForm.controls;
+        for (const controlName in controls) {
+          this.playerForm.controls[controlName].setValue(this.player[controlName]);
+        }
+      }
   }
 
   private newPlayer(playerFormValue) {
@@ -48,6 +73,9 @@ export class PlayerDialogComponent implements OnInit {
   }
 
   private editPlayer(playerFormValue) {
+
+
+
     const playerFormValueWithKey = { ...playerFormValue, $key: this.player.$key };
     const playerFormValueWithFormattedKey = { ...playerFormValue, key: this.player.$key };
     delete playerFormValueWithFormattedKey.$key;
@@ -64,21 +92,21 @@ export class PlayerDialogComponent implements OnInit {
     this.teamService.editTeam(formattedTeam);
   }
 
-  onSubmit(playerForm: NgForm) {
+  onSubmit() {
+    console.log('form status', this.playerForm);
+    const playerFormValue = { ...this.playerForm.value };
+    console.log('jugador completo', playerFormValue);
 
-    const playerFormValue = { ...playerForm.value };
-    console.log('jugador completo', playerForm);
-
-    if (playerForm.valid) {
-      console.log('leftFooted', playerFormValue.leftFooted);
-      playerFormValue.leftFooted = playerFormValue.leftFooted === '' ? false : playerFormValue.leftFooted;
-    }
+    // if (playerForm.valid) {
+    //   console.log('leftFooted', playerFormValue.leftFooted);
+    //   playerFormValue.leftFooted = playerFormValue.leftFooted === '' ? false : playerFormValue.leftFooted;
+    // }
     if (this.player) {
-      console.log('editar', this.player);
+
       this.editPlayer(playerFormValue);
 
     } else {
-      console.log('nuevo player', this.player);
+
       this.newPlayer(playerFormValue);
     }
     this.onClose();
